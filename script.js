09d1c692fd4d9f941cd84f04a926b564f32986b6
Dhruw Vaviya <dhruwvaviya123@gmail.com>
2025-04-18 10:19:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-function getFormvalue() {
+function getFormvalue(e) {
     //Write your code here
-
+	e.preventDefault();
+	const fName = document.querySelector("#fName").value;
+	const lName = document.querySelector("#lName").value;
+	alert(`${fName} ${lName}`);
+	
 }
